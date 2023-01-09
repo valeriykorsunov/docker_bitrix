b6a5d13b3a30c3c4cc380f9f65d2431e6314ec60
Валерий Корсунов <kors.vv@ya.ru>
2023-01-10 00:06:28 +0300

    +

--- down.sh
+++ down.sh
@@ -2,6 +2,8 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# не создает файла local.sql
+# нужно настроить автоматическое создание нового файла с датой (год-месяц-день)
 docker exec mysql sh -c 'exec mysqldump --databases bitrix -uroot -p"root"' > "$CURRENT_DIR/data/backup/local.sql"
 
 docker-compose down
