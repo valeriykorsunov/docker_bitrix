f8aea42d49788a2f7ab41f33900b0d2057f7bcd1
Valeriy <valeriy@magwai.ru>
2023-01-13 08:18:25 +0300

    fetch: дата для бэкапа БД

--- down.sh
+++ down.sh
@@ -2,7 +2,7 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-FNAME=$CURRENT_DIR/data/backup/local_`date +%Y%m%d`.sql
+FNAME=$CURRENT_DIR/data/backup/`date +%Y%m%d_%H%M%S`_down_local.sql
 
 # docker exec mysql sh -c 'exec mysqldump --databases bitrix -uroot -p"root"' > "$CURRENT_DIR/data/backup/local.sql"
 docker exec mysql sh -c 'exec mysqldump --databases bitrix -uroot -p"root"' > "$FNAME"
