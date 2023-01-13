80fbcab8d914273e523d3543776480870b6b4610
Valeriy <valeriy@magwai.ru>
2023-01-13 10:45:59 +0300

    +

--- down.sh
+++ down.sh
@@ -2,7 +2,7 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-FNAME=$CURRENT_DIR/data/backup/`date +%Y%m%d_%H%M%S`_down_local.sql
+FNAME=$CURRENT_DIR/data/backup/`date +%Y%m%d`_down_local.sql
 
 # docker exec mysql sh -c 'exec mysqldump --databases bitrix -uroot -p"root"' > "$CURRENT_DIR/data/backup/local.sql"
 docker exec mysql sh -c 'exec mysqldump --databases bitrix -uroot -p"root"' > "$FNAME"
