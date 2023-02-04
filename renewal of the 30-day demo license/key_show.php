d70f36dfc85b2420739e57b6704a80ffe83f5386
Валерий Корсунов <kors.vv@ya.ru>
2023-02-04 17:06:19 +0300

    +

--- renewal of the 30-day demo license/key_show.php
+++ renewal of the 30-day demo license/key_show.php
@@ -8,9 +8,9 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.
 /** @var \CDatabase $DB */
 
 echo '<p>';
-echo 'TEMPORARY_CACHE: '. TEMPORARY_CACHE;
-echo "<br>";
-global $DB;
-$q = $DB->Query('SELECT * FROM b_option WHERE NAME="admin_passwordh"');
-echo 'admin_passwordh: '. $q->Fetch()["VALUE"];
+	echo 'TEMPORARY_CACHE: '. TEMPORARY_CACHE;
+	echo "<br>";
+	global $DB;
+	$q = $DB->Query('SELECT * FROM b_option WHERE NAME="admin_passwordh"');
+	echo 'admin_passwordh: '. $q->Fetch()["VALUE"];
 echo "</p>";
