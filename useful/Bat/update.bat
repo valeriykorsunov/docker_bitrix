9090f33267f192618d987bdca6d98b113ed62fec
Valeriy Korsunov <valeriy@magwai.ru>
2023-07-21 08:31:01 +0300

    fix update.bat

--- useful/Bat/update.bat
+++ useful/Bat/update.bat
@@ -1,15 +1,30 @@
 @echo off
+@REM --------------------------------------------------------------------------------------------
+@REM Скрипт поможет собрать все файлы из последнего коммита в одну папку(директориии сохраняются)
+@REM запускать из директории, где находится .git 
+@REM --------------------------------------------------------------------------------------------
 setlocal enabledelayedexpansion
-set folder=%~dp0up22
+REM Получение текущей даты и времени
+set "datetime=%DATE%_%TIME%"
+REM Замена недопустимых символов в имени файла
+set "datetime=%datetime:/=-%"
+set "datetime=%datetime::=-%"
+set "datetime=%datetime:.=-%"
+set "datetime=%datetime:,=-%"
+REM Удаление пробелов в начале имени файла (если есть)
+set "datetime=%datetime: =%"
+REM Используйте переменную %datetime% в качестве имени файла
+echo %datetime%
+set folder=%~dp0data\update\%datetime%
 set folder=%folder:/=\%
 @REM Создаем папку "up22" в директории, где находится исполняемый .bat файл
 mkdir "%folder%"
-@REM Получаем список файлов, содержащих изменения за последние 3 коммита
+@REM Получаем список файлов, содержащих изменения за последний коммит
 for /f "delims=" %%x in ('git diff --name-only HEAD~1 HEAD') do (
     @REM Заменяем слеши в пути к файлу на обратные слеши
     set file=%%x
     set file=!file:/=\!
-    echo "%folder%\!file!"
+    @REM echo "%folder%\!file!"
 	echo F | xcopy "!file!" "%folder%\!file!" /Y /I
 )
 endlocal
